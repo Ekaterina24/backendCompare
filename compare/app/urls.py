62e82acc5d4a6ecefeb0d245c454d9388e5eefba
rykova_ed <katrykova2002@yandex.ru>
2025-05-04 15:02:33 -0700

    .

--- compare/app/urls.py
+++ compare/app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from app.views import AlgorithmsPostView, ConvertPdfView
+from app.views import AlgorithmsPostView
 
 urlpatterns = [
     path('methods/', AlgorithmsPostView.as_view(), name='methods'),
-    path("convert-pdf/", ConvertPdfView.as_view(), name="convert-pdf"),
 ]
