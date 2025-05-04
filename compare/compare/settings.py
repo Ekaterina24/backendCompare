99ae2b7867c7c8d2128c2902a3b75f55ac82886a
rykova_ed <katrykova2002@yandex.ru>
2025-05-04 10:00:52 -0700

    add host

--- compare/compare/settings.py
+++ compare/compare/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +27,7 @@ SECRET_KEY = 'django-insecure-#*ot9#ggcx9(0d@j@mxi9b%gzcgelkfg+z==&eg%l&tqyg&=j4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['https://backendcompare.onrender.com']
 
 # Application definition
 
